2b29d34562dfd1df0eab0c2d7d30326321ada9f7
Peter Kagey <peterkagey@gmail.com>
2021-04-06 15:01:35 -0700

    Update data format.

--- json_accountant.py
+++ json_accountant.py
@@ -4,11 +4,9 @@ from datetime import datetime
 # JSONAccountant reads data.json, and randomly picks an unseen value, and moves it to the seen collection.
 class JSONAccountant:
   def __init__(self):
-    self.file_location = "data.json"
-    self.data = json.loads(open(self.file_location, "r").read())
-    self.unseen_sequences = self.data["unseen"]
+    self.unseen_sequences = json.loads(open("data.json", "r").read())
 
   def get_todays_sequence(self):
-    unseen_sequences = self.unseen_sequences
     todays_index = (datetime.now() - datetime(2021,4,5)).days
-    return (self.unseen_sequences[todays_index][0], self.unseen_sequences[todays_index][1])
+    [a_number, tweet_copy] = self.unseen_sequences[todays_index][0]
+    return (a_number, tweet_copy)
